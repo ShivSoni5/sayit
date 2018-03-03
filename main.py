8391ce61444ff7f7834fcb9a56147c85e330297e
ShivSoni5 <shivsonic05@gmail.com>
2018-03-03 13:49:07 +0530

    searching query

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python
 
 import speech_recognition as sr
-import os
+import webbrowser as wb
 from termcolor import colored
+from time import sleep
 
 BOLD = "\033[1m"   #use to bold the text
 END = "\033[0m"    #use to close the bold text
 
 r = sr.Recognizer()
-while True:
-	with sr.Microphone() as source:
-		print colored(BOLD+"Say it!"+END,"red")
-		audio = r.listen(source)
+with sr.Microphone() as source:
+	print colored(BOLD+"Say your Query!"+END,"red")
+	audio = r.listen(source)
 
 
-	try:
-		sayit = "You said "+r.recognize_google(audio)
+try:
+	sayit = r.recognize_google(audio)
+	you_say = "Searching for "+sayit
+	print colored(BOLD+you_say+END,"green")
+	sleep(2)
+	wb.open("https://www.google.com/search?q="+sayit+"")
 
-	except sr.UnknownValueError:
-		sayit = "I could not understand this audio"
+except sr.UnknownValueError:
+	print("I could not understand this audio")
 
-	except sr.RequestError as e:
-		sayit = "I could not request results from Google Speech Recognition service; {0}".format(e)
+except sr.RequestError as e:
+	print("I could not request results from Google Speech Recognition service; {0}".format(e))
 
-	print colored(BOLD+sayit+END,"green")
